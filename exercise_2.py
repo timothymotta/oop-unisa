1b7490d3a51ba9c91eb70e9b380811ac1c104d7a
timothymotta <a1831126@adelaide.edu.au>
2024-02-28 14:28:15 +1030

    hiiii

---
+++ exercise_2.py
@@ -0,0 +1,15 @@
+# This Calculator class has limited functionality. It would need to at least
+# subtract, multiply and divide. Using the example code, add the appropriate functionality.
+
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+    
+    def divide(self, a, b:
+        return a / b
