ac8294cd41f93652ff73c11c822f1a0e4fef3176
timothymotta <timothy@TIMO-DELL>
2024-05-26 15:24:22 +0930

    added the classes, haven't selected one for the abstract class yet

--- ASSIGNMENT 1.py
+++ ASSIGNMENT 1.py
@@ -1,5 +1,60 @@
 import random
 
-class Test:
-    def __init__(self) -> None:
+# class for bubble tea
+class BubbleTea:
+    def __init__(self, name, size, level_of_ice, level_of_sugar, green_or_black_tea, toppings, price):
+        self.__name = name
+        self.__size = size
+        self.__level_of_ice = level_of_ice
+        self.__level_of_sugar = level_of_sugar
+        self.__green_or_black_tea = green_or_black_tea
+        self.__toppings = toppings
+        self.__price = price
+
+
+# class for fruit tea
+class FruitTea(BubbleTea):
+    def __init__(self):
+        super().__init__()
+pass
+
+# class for milk tea
+class MilkTea(BubbleTea):
+    def __init__(self):
+        super().__init__()
+pass
+
+# class for sparkling tea
+class SparklngTea(BubbleTea):
+    def __init__(self):
+        super().__init__()
         pass
+    
+# class for hot tea
+class HotTea(BubbleTea):
+    def __init__(self):
+        super().__init__()
+        pass
+
+# class for frozen tea
+class FrozenTea(BubbleTea):
+    def __init__(self):
+        super().__init__()
+        pass
+
+# class for topping
+class Topping(BubbleTea):
+    def __init__(self):
+        super().__init__()
+        pass
+
+# class for store, contains functions that order and/or create tea
+class Store:
+    def __init__(self):
+        # function for ordering tea
+        def ordering_tea():
+            pass
+        
+        # function for creating tea
+        def creating_tea():
+         pass
