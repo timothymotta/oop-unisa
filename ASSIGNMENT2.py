f3771c2f88b1c1a1854f140b2a597f0043cc9b12
timothymotta <a1831126@adelaide.edu.au>
2024-06-18 18:53:18 +0930

    fixing display

--- ASSIGNMENT2.py
+++ ASSIGNMENT2.py
@@ -54,10 +54,10 @@ class BubbleTea(ABC):
         pass
 
     def __str__(self):
-        toppings_str = ', '.join([str(t) for t in self.toppings])
-        return (f'{self.name} ({self.size}, {self.green_or_black_tea} tea, '
-                f'Ice: {self.level_of_ice}, Sugar: {self.level_of_sugar}, '
-                f'Toppings: {toppings_str})')
+        toppings_str = ", ".join([str(t) for t in self.toppings])
+        return (f"{self.name} ({self.size}, {self.green_or_black_tea} tea, "
+                f"Ice: {self.level_of_ice}, Sugar: {self.level_of_sugar}, "
+                f"Toppings: {toppings_str})")
 
     # function for adding topping
     def add_topping(self, topping):
@@ -87,7 +87,7 @@ class FruitTea(BubbleTea):
             price = price + 0.85
         elif self.size == "Large":
             price = price + 1.2
-        elif self.size == 'Small':
+        elif self.size == "Small":
             price = price + 0
         for topping in self.toppings:
             price = price + topping.price
@@ -101,11 +101,11 @@ class MilkTea(BubbleTea):
     # calculating price for milk tea based on size and topping choices
     def calculate_price(self):
         price = self.base_tea_price + 0.29
-        if self.size == 'Medium':
+        if self.size == "Medium":
             price = price + 0.85
-        elif self.size == 'Large':
+        elif self.size == "Large":
             price = price + 1.2
-        elif self.size == 'Small':
+        elif self.size == "Small":
             price = price + 0
         for topping in self.toppings:
             price = price + topping.price 
@@ -119,11 +119,11 @@ class SparklingTea(BubbleTea):
     # calculating price for sparkling tea based on size and topping choices
     def calculate_price(self):
         price = self.base_tea_price + 0.19
-        if self.size == 'Medium':
+        if self.size == "Medium":
             price = price + 0.85
-        elif self.size == 'Large':
+        elif self.size == "Large":
             price = price + 1.2
-        elif self.size == 'Small':
+        elif self.size == "Small":
             price = price + 0
         for topping in self.toppings:
             price = price + topping.price
@@ -136,11 +136,11 @@ class HotTea(BubbleTea):
 
     def calculate_price(self):
         price = self.base_tea_price + 0.24
-        if self.size == 'Medium':
+        if self.size == "Medium":
             price = price + 0.85
-        elif self.size == 'Large':
+        elif self.size == "Large":
             price = price + 1.2
-        elif self.size == 'Small':
+        elif self.size == "Small":
             price = price + 0
         for topping in self.toppings:
             price = price + topping.price
@@ -153,11 +153,11 @@ class FrozenTea(BubbleTea):
 
     def calculate_price(self):
         price = self.base_tea_price + 3.0
-        if self.size == 'Medium':
+        if self.size == "Medium":
             price = price + 0.85
-        elif self.size == 'Large':
+        elif self.size == "Large":
             price = price + 1.2
-        elif self.size == 'Small':
+        elif self.size == "Small":
             price = price + 0
         for topping in self.toppings:
             price = price + topping.price
@@ -181,44 +181,44 @@ class Topping:
     def __str__(self):
         return f'{self.name} (${self.price:.2f})'
 
-# class for store, contains functions that order and/or create tea
+# class for store, contains functions that order and/or create tea, contains dictoionary for prices and empty list for orders
 class Store:
     def __init__(self):
         self.earnings = 0.0
         self.orders = []
         self.prices = {
-            'BASE PRICE': 4.5,
-            'SIZE OF TEA': {
-                'Small': 0.0,
-                'Medium': 0.85,
-                'Large': 1.2,
+            "BASE PRICE": 4.5,
+            "SIZE OF TEA": {
+                "Small": 0.0,
+                "Medium": 0.85,
+                "Large": 1.2,
             },
-            'TYPE OF TEA': {
-                'Fruity': 0.15,
-                'Milky': 0.29,
-                'Sparkling': 0.19,
-                'Hot': 0.24,
-                'Frozen': 3.0,
+            "TYPE OF TEA": {
+                "Fruity": 0.15,
+                "Milky": 0.29,
+                "Sparkling": 0.19,
+                "Hot": 0.24,
+                "Frozen": 3.0,
             },
-            'FRUITS': {
-                'Mango': 0.3, 'Lychee': 0.1, 'Apple': 0.2,
-                'Grape': 0.4, 'Melon': 0.5, 'Grapefruit': 0.1,
-                'Lemon': 0.3, 'Guava': 0.2, 'Passionfruit': 0.25,
-                'Strawberry': 0.2, 'Pomegranate': 0.45, 'Peach': 0.1,
-                'Tropical': 0.65, 'Watermelon': 0.35,
+            "FRUITS": {
+                "Mango": 0.3, "Lychee": 0.1, "Apple": 0.2,
+                "Grape": 0.4, "Melon": 0.5, "Grapefruit": 0.1,
+                "Lemon": 0.3, "Guava": 0.2, "Passionfruit": 0.25,
+                "Strawberry": 0.2, "Pomegranate": 0.45, "Peach": 0.1,
+                "Tropical": 0.65, "Watermelon": 0.35,
             },
-            'FLAVOURS': {
-                'Mint Choc': 0.3, 'Thai': 0.35, 'Salted Caramel': 0.2,
-                'Roasted': 0.4, 'Earl Grey': 0.5, 'Vanilla': 0.1,
-                'Assam': 0.3, 'Hazelnut': 0.2, 'Oolong': 0.25,
-                'Jasmine': 0.2, 'Matcha': 0.45, 'Taro': 0.65,
-                'Chocolate': 0.4,
+            "FLAVOURS": {
+                "Mint Choc": 0.3, "Thai": 0.35, "Salted Caramel": 0.2,
+                "Roasted": 0.4, "Earl Grey": 0.5, "Vanilla": 0.1,
+                "Assam": 0.3, "Hazelnut": 0.2, "Oolong": 0.25,
+                "Jasmine": 0.2, "Matcha": 0.45, "Taro": 0.65,
+                "Chocolate": 0.4,
             },
-            'TOPPINGS': {
-                'Custard': 1.0, 'Mousse': 2.4, 'Pearls': 1.2,
-                'Cookie Crumb': 0.75, 'Mixed Jellies': 0.35, 'Herbal Jelly': 0.45,
-                'Coconut Jelly': 0.5, 'Aloe Vera': 0.7, 'Mango Popping Pearls': 0.4,
-                'Strawberry Popping Pearls': 0.4, 'Apple Popping Pearls': 0.4,
+            "TOPPINGS": {
+                "Custard": 1.0, "Mousse": 2.4, "Pearls": 1.2,
+                "Cookie Crumb": 0.75, "Mixed Jellies": 0.35, "Herbal Jelly": 0.45,
+                "Coconut Jelly": 0.5, "Aloe Vera": 0.7, "Mango Popping Pearls": 0.4,
+                "Strawberry Popping Pearls": 0.4, "Apple Popping Pearls": 0.4,
             }
         }
 
@@ -236,8 +236,8 @@ class Store:
 
     # string converter 
     def __str__(self):
-        return f'Total Earnings: \n${self.earnings:.2f}, \nTotal Orders: \n{len(self.orders)}\n'
-
+        return f'Total Earnings: \n${self.earnings:.2f}, \n\nTotal Orders: \n{len(self.orders)}\n'
+    
 def main():
     store = Store()
 
@@ -251,7 +251,7 @@ def main():
     print("|                    ░█▄▄█ ─▀▄▄▀ ░█▄▄█ ░█▄▄█ ░█▄▄█ ░█▄▄▄ 　 ─░█── ░█▄▄▄ ░█─░█                     |\n")
     print("|=================================================================================================|")
 
-    store.order_tea(FruitTea, "Tropical Fruit Tea", "Large", "Regular", "Full", "Green", [mango_topping, lychee_topping])
+    store.order_tea(FruitTea, "Tropical Fruit Tea", "Large", "Regular", "Full", "Green", [strawberry_topping, lychee_topping])
     store.order_tea(MilkTea, "Classic Milk Tea", "Medium", "Less", "Half", "Black", [lychee_topping])
     store.order_tea(SparklingTea, "Sparkling Lemon Tea", "Small", "Full", "Full", "Green", [mango_topping])
     store.order_tea(HotTea, "Hot Earl Grey Tea", "Large", "None", "None", "Black", [])
@@ -259,8 +259,6 @@ def main():
     
     store.order_tea(FruitTea, "Tropical Fruit Tea", "Large", "Regular", "Full", "Green", [mango_topping, lychee_topping])
 
-    
-    
     # display store
     print(store)
     store.view_order_history()
